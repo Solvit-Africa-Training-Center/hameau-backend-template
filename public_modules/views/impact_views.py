1c7af70c08bedc5b2548b6da19300ae0f16a8161
go-arnold <arnoldciku@gmail.com>
2026-02-24 12:24:55 +0000

    Update RapiDoc template and modify URL patterns for improved documentation

--- public_modules/views/impact_views.py
+++ public_modules/views/impact_views.py
@@ -1,10 +1,11 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
 
 from public_modules.serializers.impact_serializers import ImpactStatsSerializer
 
-
+@extend_schema(tags=["Public Modules"])
 class ImpactStatsView(APIView):
     permission_classes = [AllowAny]
 
